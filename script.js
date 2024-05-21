e37c1a677de29bd9bdecd6283a50414e4d1e1e9b
DavikChct <davikchaudharyawc@gmail.com>
2024-05-21 15:12:11 +0530

    script bug fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let captchaChecked = false;
 
-function beforeSubmit(){
-    if(captchaChecked(event)){
+function beforeSubmit(event){
+    if(captchaChecked()){
         let outputDate = document.querySelector(".outputDate");
         let inputDate = document.querySelector(".inputDate");
         console.log('inputDate.value',inputDate.value);
