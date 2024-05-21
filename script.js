01d6558d965ebcb995c47454ad4b5b0fccc92050
DavikChct <davikchaudharyawc@gmail.com>
2024-05-21 15:07:13 +0530

    recaptcha

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+let captchaChecked = false;
+
 function beforeSubmit(){
-    let outputDate = document.querySelector(".outputDate");
-    let inputDate = document.querySelector(".inputDate");
-    console.log('inputDate.value',inputDate.value);
-    
-    let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
-    outputDate.value = formattedDate;
+    if(captchaChecked(event)){
+        let outputDate = document.querySelector(".outputDate");
+        let inputDate = document.querySelector(".inputDate");
+        console.log('inputDate.value',inputDate.value);
+        
+        let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+        outputDate.value = formattedDate;
+    }else{
+        alert('Please check the reCaptcha field to submit');
+        event.preventDefault();
+    }
 }
 
 
  function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+ function captchasuccess(){
+    captchaChecked = true;
+ }
